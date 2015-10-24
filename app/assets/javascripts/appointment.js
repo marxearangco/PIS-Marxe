3c1357b6b29dc1d361f9077b5c542a13aba77628
marxearangco <marcoarangco@gmail.com>
2015-10-24 23:04:23 +0700

    PIS initiated

---
+++ app/assets/javascripts/appointment.js
@@ -0,0 +1,6 @@
+// $('#tblappointment').DataTable({
+//     sPaginationType: "full_numbers",
+//     bJqueryUI: true
+//     destroy: true
+//     // sDom: '<"H"Tl>tS<"F"ip>'
+//   });
--- app/assets/javascripts/patient.js
+++ app/assets/javascripts/patient.js
@@ -18,16 +18,17 @@ $('.thumbnail').hover(
 // //         sAjaxSource: $('#tblpatients').data('source')
 
 // jQuery(function($) {
-$('#tblpatients').DataTable({
-	sPaginationType: "full_numbers",
-	bJqueryUI: true,
-	});
-  // ajax: ...,
-  // autoWidth: false,
-  // processing: true,
-  // serverSide: true,
+// $('#tblpatients').DataTable({
+// 	sPaginationType: "full_numbers",
+// 	bJqueryUI: true,
+//   destroy: true
+// 	});
+//   // ajax: ...,
+//   // autoWidth: false,
+//   // processing: true,
+//   // serverSide: true,
 
-  // Optional, if you want full pagination controls.
-  // Check dataTables documentation to learn more about available options.
-  // http://datatables.net/reference/option/pagingType
+//   // Optional, if you want full pagination controls.
+//   // Check dataTables documentation to learn more about available options.
+//   // http://datatables.net/reference/option/pagingType
 // });
