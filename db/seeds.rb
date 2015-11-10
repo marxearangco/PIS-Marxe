83523917350e7009e718c95f78865ae96db6e400
marxearangco <marcoarangco@gmail.com>
2015-11-10 17:06:57 +0800

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-case Rails.env
-when "development"
-	User.create(:email => 'marcoarangco@gmail.com', :password => 'p@$$w0rd', :password_confirmation => 'p@$$w0rd', :username => 'administrator', :role => 'admin')
-when "production"
-	User.create(:email => ENV['GMAIL_USERNAME'], :password => ENV['GMAIL_PASSWORD'], :password_confirmation => ENV['GMAIL_PASSWORD'], :username => 'administrator', :role => 'admin')
-end
+User.create(:email => 'marcoarangco@gmail.com', :password => 'p@$$w0rd', :password_confirmation => 'p@$$w0rd', :username => 'administrator', :role => 'admin')
